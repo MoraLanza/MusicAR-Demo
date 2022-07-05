e48cd7b630549a94ac19330287c534bd7b4990bf
Laura Alvarez M <lalvar16@eafit.edu.co>
2022-07-04 19:11:17 -0500

    Ajustes y creacion de páginas

--- G4Musicar/app.js
+++ G4Musicar/app.js
@@ -9,6 +9,18 @@ app.use(express.static(publicPath));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 });
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/contacto.html'))
+});
+app.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/faq.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/registro.html'))
+});
 
 
 app.listen(3000, () =>
