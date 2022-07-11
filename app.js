70781bdbf65edb8899fae0ed3f119faaf11a3849
unknown <fabianvega140882@gmail.com>
2022-07-10 22:05:26 -0300

    cambios

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+});
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/contacto.html'))
+});
+app.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/faq.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/registro.html'))
+});
+
+
+app.listen(3000, () =>
+console.log('http://localhost:3000'));
