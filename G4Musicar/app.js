85c07e7d7f60c437b9a70e5c516ea67d4fac2699
lidia <lidia@codingco.io>
2022-07-14 14:27:27 -0300

    header done

--- G4Musicar/app.js
+++ G4Musicar/app.js
@@ -6,8 +6,6 @@ const publicPath = path.join(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/product-detail.html'))
 });
