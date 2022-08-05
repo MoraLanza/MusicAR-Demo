fe09dd154dccb183dd80c8516e0ba71a38fe320d
MoraLanza <dalilamoru@gmail.com>
2022-08-04 23:57:35 -0300

    crud

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let productController = {
-    productDetail: function(req, res){
-         res.render('products/product-detail');
+    index: (req, res) => {
+        res.render('index', {products})
+    },
+    detail: (req, res) => {
+         res.render('./products/product-detail');
+    },
+    create: (req, res) => {
+        res.render('./products/crear-evento');
+    },
+    store: (req, res) => {
+        const productsClone = products;
+		const newProduct = {
+			id: req.body.name,
+			tipoDeShow: req.body.tipoDeShow,
+			clase: req.body.clase,
+            artista: req.body.artista,
+			descripcion: req.body.descripcion,
+			pais: req.body.pais,
+            provincia: req.body.provincia,
+			ciudad: req.body.ciudad,
+			escenario: req.body.escenario,
+            direccion: req.body.direccion,
+			fechaFuncion1: req.body.fechaFuncion1,
+			horario1: req.body.horario1,
+            precio1: req.body.precio1,
+			price: req.body.price,
+			description: req.body.description,
+			image: req.file ? req.file.filename : null
+		}
+
+		productsClone.push(newProduct);
+		fs.writeFileSync(productsFilePath, JSON.stringify(productsClone, null, ' '));
+		res.redirect('/products');
+    },
+    edit: (req, res) => {
+        const product = products.find(element => element.id == req.params.id); 
+        res.render('/products/editar-evento', {product});
+    },
+    update: (req, res) => {
+
+    },
+    delete: (req, res) => {
+
     }
-}
+
+};
 
 module.exports = productController;
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,9 +1,31 @@
 let express = require('express');
+const multer = require('multer');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/products')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+
+const upload = multer ({storage});
 
 const productController = require('../controllers/productController');
 
-router.get('/product-detail', productController.productDetail);
+router.get('/', productController.index)
+
+router.get('/detail', productController.detail);
+
+router.get('/create', productController.create);
+router.post('/create',upload.single('image'), productController.store);
+
+router.get('/:id/edit', productController.edit);
+router.put('/:id/edit', productController.update);
+
+router.delete('/:id/delete', productController.delete);
 
 
 module.exports = router;
