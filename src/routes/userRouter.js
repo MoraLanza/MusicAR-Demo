32a0ada69a3840f834ef5d7d89f4ad2338b1382d
MoraLanza <dalilamoru@gmail.com>
2022-08-16 23:41:15 -0300

    .

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,8 @@
 let express = require('express');
 const multer = require('multer');
+const { body } = require('express-validator');
+
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -12,15 +15,31 @@ const storage = multer.diskStorage({
       }
 });
 
-
 const upload = multer ({storage});
 
-const userController = require('../controllers/userController');
+const validator = [
+    body('name').notEmpty()
+    .isLength({min: 2}).withMessage('Debe ingresar un nombre'),
+
+    body('lastName').notEmpty()
+    .isLength({ min: 2 }).withMessage('Debe seleccionar un apellido'),
+
+    body('email').notEmpty()
+    .isEmail().withMessage('Debe ingresar un email valido.'),
+
+    body('username').notEmpty().withMessage('Debe ingresar un nombre')
+    .isLength({min: 5, max: 16}).withMessage('El nombre de usuario debe tener mínimo 5 caracteres, máximo 16.'),
+
+    body('password').notEmpty().withMessage('Debe ingresar una contraseña')
+    .isLength({min: 8, max: 16}).withMessage('La contraseña debe tener mínimo 8 caracteres, máximo 16.'),
+
+    body
+];
 
 router.get('/shopping-cart', userController.cart);
 router.get('/login', userController.login);
 
 router.get('/register', userController.register);
-router.post('/register', upload.single('image-user'), userController.store);
+router.post('/register', upload.single('imageUser'), userController.store);
 
 module.exports = router;
