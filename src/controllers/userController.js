7b9b1f5f3bd2c35105c0a74cf3434b3264df1045
MoraLanza <dalilamoru@gmail.com>
2022-09-29 17:37:58 -0300

    user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,16 @@
 
 const bcryptjs = require('bcryptjs');
-const User = require('../models/User');
+
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require('sequelize');
+
 const Users = db.User;
 
 const { validationResult } = require('express-validator');
 
 
 let userController = {
-    create: (req, res) => {
-        Users.create(
-            {
-                id: req.body.id,
-                name: req.body.name,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: bcryptjs.hashSync(req.body.password, 10),
-                roles_id: req.body.roles_id,
-                citys_id: req.body.citys_id,
-                imageUser: req.file ? req.file.filename : null,
-            }
-        )
-        .then(() => {
-            return res.redirect('/users')
-        })
-        .catch(error => res.send(error))
-    },
-
-    edit: (req, res) => {
-        const userId = req.params.id;
-        const promUsers = Users.findByPk(userId, { include: ['user'] });
-        Promise
-            .all([promUsers])
-            .then(([promUsers]) => {
-                return res.render('edit-event', { Movie, allGenres, allActors })
-            })
-            .catch(error => res.send(error))
-    },
     
     cart: function (req, res) {
        return res.render("users/shopping-cart");
@@ -44,8 +19,14 @@ let userController = {
        return res.render("users/login");
     },
 
-    loginProcess: (req, res) => {
-        let userToLogin = User.findFirstByField("email", req.body.email);
+    loginProcess: async (req, res) => {
+        try{
+
+        let userToLogin = await Users.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
        
         if (userToLogin) {
             let truePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
@@ -76,6 +57,9 @@ let userController = {
                 }
             }
         })
+    } catch (error) {
+        res.send(error)
+    }
     },
     
     logout: (req, res) => {
@@ -87,7 +71,8 @@ let userController = {
     register: function (req, res) {
        return res.render("users/register");
     },
-    store: function (req, res) {
+    store: async function (req, res) {
+       try {
         const resultValidation = validationResult(req);
 
         if (resultValidation.errors.length > 0) {
@@ -96,7 +81,11 @@ let userController = {
                 oldData: req.body
             });
         }
-        let userInDB = User.findFirstByField('email', req.body.email);
+        let userInDB = await Users.findOne({
+            where:{
+                email: req.body.email
+            }
+    });
 
         if (userInDB) {
             return res.render('users/login', {
@@ -117,10 +106,108 @@ let userController = {
             password: bcryptjs.hashSync(req.body.password, 10),
             imageUser: req.file ? req.file.filename : null,
         }
-        let userCreated = User.create(userToCreate);
+        let userCreated = await Users.create(userToCreate);
 
-      return  res.redirect('/');
+        return  res.redirect('/');
+
+    } catch (error) {
+        res.send(error)
+    }
     }
 }
 
 module.exports = userController;
+
+
+
+// const { validationResult } = require('express-validator');
+
+
+// let userController = {
+//     cart: function (req, res) {
+//        return res.render("users/shopping-cart");
+//     },
+//     login: function (req, res) {
+//        return res.render("users/login");
+//     },
+
+//     loginProcess: (req, res) => {
+//         let userToLogin = User.findFirstByField("email", req.body.email);
+       
+//         if (userToLogin) {
+//             let truePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+            
+//             if (truePassword) {
+//                 delete userToLogin.password;
+//                 req.session.userLogged = userToLogin;
+
+//                 if (req.body.remember_name) {
+//                     res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 360 })
+//                 }
+//              return res.redirect('/');
+                
+//             }
+//             return res.render('users/login', {
+//                 errors: {
+//                     email: {
+//                         msg: 'Email o contraseña invalidos'
+//                     }
+//                 }
+//             })
+//         }
+
+//         return res.render('users/login', {
+//             errors: {
+//                 email: {
+//                     msg: 'Email o contraseña invalidos'
+//                 }
+//             }
+//         })
+//     },
+    
+//     logout: (req, res) => {
+//         res.clearCookie('userEmail');
+//         req.session.destroy();
+//         return res.redirect('/');
+//     },
+
+//     register: function (req, res) {
+//        return res.render("users/register");
+//     },
+//     store: function (req, res) {
+//         const resultValidation = validationResult(req);
+
+//         if (resultValidation.errors.length > 0) {
+//             return res.render('users/register', {
+//                 errors: resultValidation.mapped(),
+//                 oldData: req.body
+//             });
+//         }
+//         let userInDB = User.findFirstByField('email', req.body.email);
+
+//         if (userInDB) {
+//             return res.render('users/login', {
+//                 errors: {
+//                     email: {
+//                         msg: 'Este email ya está en uso'
+//                     }
+//                 },
+//                 oldData: req.body
+//             });
+//         }
+
+//         let userToCreate = {
+//             name: req.body.name,
+//             lastName: req.body.lastName,
+//             email: req.body.email,
+//             username: req.body.username,
+//             password: bcryptjs.hashSync(req.body.password, 10),
+//             imageUser: req.file ? req.file.filename : null,
+//         }
+//         let userCreated = User.create(userToCreate);
+
+//       return  res.redirect('/');
+//     }
+// }
+
+// module.exports = userController;
