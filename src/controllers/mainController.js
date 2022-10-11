b4898ae12307a06575635ce0dfdd8bbd2c23a42a
MoraLanza <dalilamoru@gmail.com>
2022-10-11 11:22:46 -0300

    main controller

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -20,9 +20,12 @@ let mainController = {
             const teaters = await Teaters.findAll();
             const citys = await Citys.findAll();
             const tickets = await Tickets.findAll();
-
+            const categories = await Categories.findAll();
+            
 
             if (req.session.userLogged) {
+
+                const user = await Users.findByPk(req.session.userLogged.id);
 
                 allEvents = await Events.findAll({
                     where: {
@@ -30,13 +33,20 @@ let mainController = {
                     }
                 });
 
+                cityEvents = await Events.findAll({
+                    where: {
+                        city_id: req.session.userLogged.city_id
+                    }
+                })
+
+                return res.render('index', { allEvents, functions, teaters, citys, tickets, user, categories, cityEvents });
             } else {
 
                 allEvents = await Events.findAll();
 
-
+                return res.render('index', { allEvents, functions, teaters, citys, tickets, categories });
             }
-            return res.render('index', { allEvents, functions, teaters, citys, tickets });
+            
 
         } catch (error) {
             res.send(error)
@@ -50,67 +60,42 @@ let mainController = {
         res.render("faq");
     },
     searchBar: async function (req, res) {
-        try {
-            let allEvents;
-            const functions = await Functions.findAll();
-            const teaters = await Teaters.findAll();
-            const citys = await Citys.findAll();
-            const categories = await Categories.findAll();
+        // try {
+        //     let allEvents;
+        //     const functions = await Functions.findAll();
+        //     const teaters = await Teaters.findAll();
+        //     const citys = await Citys.findAll();
+        //     const categories = await Categories.findAll();
 
 
-            if (req.session.userLogged) {
+        //     if (req.session.userLogged) {
 
-                allEvents = await Events.findAll({
-                    where: {
-                        category_id: req.session.userLogged.category_id
-                    }
-                });
+        //         allEvents = await Events.findAll({
+        //             where: {
+        //                 category_id: req.session.userLogged.category_id
+        //             }
+        //         });
 
-            } else {
+        //     } else {
 
-                allEvents = await Events.findByPk(req.params.id,
-                    {
-                        include: [{ association: 'citys' },
-                                  { association: 'teaters' },
-                                  { association: '' }]
-                    })
-            };
+        //         allEvents = await Events.findByPk(req.params.id,
+        //             {
+        //                 include: [{ association: 'citys' },
+        //                           { association: 'teaters' },
+        //                           { association: '' }]
+        //             })
+        //     };
 
 
 
-            return res.render('index', { allEvents, functions, teaters, citys, tickets });
+        //     return res.render('index', { allEvents, functions, teaters, citys, tickets });
 
-        } catch (error) {
-            res.send(error)
-        }
+        // } catch (error) {
+        //     res.send(error)
+        // }
     }
 }
 
 module.exports = mainController;
 
-// const path = require('path');
-// const fs = require('fs');
-// const { localsName } = require('ejs');
 
-// const getProducts = () => {
-//     const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-//     const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-//     return products;
-// };
-
-// let mainController = {
-//     index: function (req, res) {
-//         const featured = getProducts().filter(product => product.class === 'destacados');
-// 		const searched = getProducts().filter(product => product.class === 'buscados');
-//         const selected = getProducts().filter(product => product.class === 'seleccionados');
-//         res.render('index',{selected, featured, searched} );
-//     },
-//     contact: function(req, res){
-//         res.render('contact');
-//     },
-//     faq: function(req, res){
-//         res.render("faq");
-//     }
-// }
-
-// module.exports = mainController;
