37322873b6cf3bfe7d0856659ecb4956e4f50b6c
gotyl <lidia@codingco.io>
2022-11-08 19:54:44 -0300

    dashboard sidebar color changed

--- dashboard/src/components/SideBar.js
+++ dashboard/src/components/SideBar.js
@@ -15,7 +15,7 @@ function SideBar(){
     return(
         <React.Fragment>
             {/*<!-- Sidebar -->*/}
-            <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion bg-red" id="accordionSidebar">
+            <ul className="navbar-nav bg-gradient-color sidebar sidebar-dark accordion bg-red" id="accordionSidebar">
 
                 {/*<!-- Sidebar - Brand -->*/}
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
